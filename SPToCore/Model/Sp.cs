59386ede12e63044f267f2044d93d8d7de092bd5
Eric Kuijpers <egm.kuijpers1@avans.nl>
2020-09-24 14:21:23 +0200

    Add project files.

---
+++ SPToCore/Model/Sp.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace SPToCore.Model
+{
+    public class Sp
+    {
+        public string Name { get; set; }
+        public string Schema { get; set; }
+                
+        public List<SpParam> Params = new List<SpParam>();
+        public List<SpResultElement> Results = new List<SpResultElement>();
+
+        public string GetMethodDefinitionAsync() {
+            if (this.Results.Count == 0)
+            {
+                return $@"public void {this.Name}";
+            }
+            else {
+                return $@"public async Task<List<{this.Name}Result>> {this.Name}Async";
+            }
+        }
+        public string GetMethodDefinitionSync()
+        {
+            if (this.Results.Count == 0)
+            {
+                return "";
+            }
+            else
+            {
+                return $@"public List<{this.Name}Result> {this.Name}Sync";
+            }
+        }
+    }
+
+}
